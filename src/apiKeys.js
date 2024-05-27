4fb0d95691b4d629a0682010855dfb3b7aaa38e6
Teju-Gowda <144135210+Teju-Gowda@users.noreply.github.com>
2024-05-27 22:46:02 +0530

    Update apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "aeef2a0752623662c1e78c4b484ac2a4",
   base: "https://api.openweathermap.org/data/2.5/",
 };
